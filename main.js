b51a3b2c312c985da6ebfb32d44999aa3e2062ac
Mandarin <45719847+gimartmart@users.noreply.github.com>
2022-02-13 22:38:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ module.exports = (key) => new Promise((resolve, reject) => {
 
     if (!key) {
         console.log(`No keywords provided\nList of endpoint \n`);
-        return get(`http://discord-holo-api.ml/api`).then((res) => {
+        return get(`http://discord-holo-api.ml:8080/api`).then((res) => {
             resolve(res.data.endpoint)
         });
     }
 
     if(key === "endpoint"){
-        return get(`http://discord-holo-api.ml/api`).then((res) => {
+        return get(`http://discord-holo-api.ml:8080/api`).then((res) => {
             resolve(res.data.endpoint)
         });
     }
-    return get(`http://discord-holo-api.ml/api/${key}`).then((res) => {
+    return get(`http://discord-holo-api.ml:8080/api/${key}`).then((res) => {
         resolve(res.data.url);
     }).catch(error => {
         if (error.response.status === 404) {
             console.log(`Availabled endpoint \n`);
-            return get(`http://discord-holo-api.ml/api/`).then((res) => {
+            return get(`http://discord-holo-api.ml:8080/api/`).then((res) => {
                 reject(res.data.endpoint)
             });
         } else {
